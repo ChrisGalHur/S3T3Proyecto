4c35ff64e5b72f2eda32cbd6a7b0f272154553c3
pilar-am <pilimorenoperez@gmail.com>
2022-12-12 09:31:25 +0100

    Actualización 12/12

--- src/Proyecto/Main.java
+++ src/Proyecto/Main.java
@@ -2,99 +2,122 @@ package Proyecto;
 
 
 import java.io.IOException;
-import java.util.Scanner;
 
 public class Main {
 
-	
 	public static void main(String[] args) {
-Scanner entrada = new Scanner(System.in);
 		
-		//llamada a los objetos
-		Floristeria llamadaObjetosFloristeria = new Floristeria();
+		Ticket ticket = new Ticket();
 		FloristeriaContenedor llamadaObjetosFloristeriaContenedor = new FloristeriaContenedor();
-		
-
-		int opcion = 0;
+		Floristeria floristeria;
+		int opcion = 0, opcionFloristeria = 0, opcionProductos = 0;
 		
 		do {
-
 			try {
-
-				System.out.println(" \n" 
-						+ "Escoge una opción: \n" 
-						+ "1. Menú floristería. \n"
-						+ "2. Menú de productos. \n"
-						+ "3. Mostrar stock. \n"
-						+ "4. Mostrar cantidades en stock. \n"
-						+ "5. Mostrar valor total de la floristería. \n"
-						+ "6. Crear Tickets. \n"
-						+ "7. Mostrar ventas anteriores. \n"
-						+ "8. Ganancias en ventas. \n"
-						+ "0. Salir.");
-
-				opcion = Integer.parseInt(entrada.nextLine());
-
+				opcion = Menu.menuFloristeria();
 				switch (opcion) {
-
 				case 1:
-					System.out.println("MENÚ FLORISTERÍA");
-					llamadaObjetosFloristeriaContenedor.menuFloristería();
+					System.out.println("CREAR FLORISTERÍA");
+					llamadaObjetosFloristeriaContenedor.crearFloristeria();
 					break;
-
 				case 2:
-					System.out.println("MENÚ DE PRODUCTOS");
-					//llamadaObjetosFloristeria.menuProductos();
+					System.out.println("MOSTRAR FLORISTERÍAS");
+					llamadaObjetosFloristeriaContenedor.mostrarFloristerias();
 					break;
-
 				case 3:
-					System.out.println("MOSTRAR STOCK");
-					llamadaObjetosFloristeria.stockFloristeria();
+					System.out.println("ELIMINAR FLORISTERÍA");
+					llamadaObjetosFloristeriaContenedor.eliminarFloristeria();
 					break;
-
 				case 4:
-					System.out.println("MOSTRAR CANTIDADES EN STOCK");
-					llamadaObjetosFloristeria.printarStockAmbCuantitats();
-					break;
+					System.out.println("OPCIONES DE FLORISTERÍA");
+					floristeria = llamadaObjetosFloristeriaContenedor.devuelveFloristeria();
+					do {
+						opcionFloristeria = Menu.menuOpcionesFloristeria();
+						switch (opcionFloristeria) {
+							case 1:
+								//Sería lo mismo que crear ticket?
+								break;
+							case 2:
+								//Mostrar stock
+								//floristeria.stockFloristeria();
+								break;
+							case 3:
+								
+								break;
+							case 4:
+								
+								break;
+							case 5:
+								//Crear los tickets, compra
+								do {
+									opcionProductos = Menu.menuProductos();
+									switch(opcionProductos) {
+									case 1:
+										Arbol arbol = floristeria.anadirArbol();
+										ticket.arbolesTicket(arbol);
+										break;
+									case 2:
+										
+										break;
+									case 3:
+										Decoracion decoracion = floristeria.anadirDecoracion();
+										ticket.decoracionTicket(decoracion);
+										break;
+									case 4:
+										
+										break;
+									case 5:
+										Flor flor = floristeria.anadirFlor();
+										ticket.floresTicket(flor);
+										break;
+									case 6:
+										
+										break;
+									case 7:
+										ticket.verTicket();
+										break;
+									case 0:
+										//Para imprimir el ticket
+										try {
+											ticket.imprimirTicket();
+										} catch (IOException e) {
+											e.printStackTrace();
+										}
+										//Almacenar ticket en hasmap
+										ticket.almacenarTicket();
+										//aquí el ticket tendría que quedar vacío
+										ticket.eliminarTicket();
+										break;
+									default:
+						
+									}
+								} while(opcionProductos != 0);
+								break;
+							case 6:
+								
+								break;
+							case 7:
+								
+								break;
+							case 0:
+								System.out.println("Saldría del menú secundario ??");
+								break;
+							default:
+								System.out.println("Número no correcto");
+						}
+					} while (opcionFloristeria != 0);
 					
-				case 5:
-					System.out.println("MOSTRAR VALOR TOTAL DE LA FLORISTERIA");
-					//llamada al método 
 					break;
-					
-				case 6:
-					System.out.println("CREAR TICKETS");
-					//llamadaObjetosFloristeria.crearTickets();
-					break;
-					
-				case 7:
-					System.out.println("MOSTRAR VENTAS ANTERIORES");
-					//llamada al método
-					break;
-					
-				case 8:
-					System.out.println("GANANCIAS EN VENTAS");
-					//llamada al método
-					break;
-					
 				case 0:
-					System.out.println("Adios.");
+					System.out.println("Hasta luego...");
 					break;
-
-				default:
-					System.out.println("ERROR opción no aceptada.");
-					break;
-
 				}
-
-			} catch (Exception ex) {
+			}catch (Exception ex) {
 				System.out.println("ERROR comando introducido no valido.");
 			}
-
 		} while (opcion != 0);
-
 	}
-
+	
 }
 
 
---
+++ src/Proyecto/Menu.java
@@ -0,0 +1,99 @@
+package Proyecto;
+
+import java.util.Scanner;
+
+public class Menu {
+
+	static int menuFloristeria() {
+		Scanner entrada = new Scanner(System.in);
+		int opcion;
+		System.out.println(" \n"
+				+ "PANEL DE CONTROL DE FLORISTERIAS: \n" 
+				+ "1. Crear floristerías. \n" 
+				+ "2. Mostrar floristerías. \n"
+				+ "3. Eliminar floristerías. \n"
+				+ "4. Opciones de floristería \n" 
+				+ "0. Salir del programa \n");
+		
+		opcion = Integer.parseInt(entrada.next());
+		
+		return opcion;
+	}
+	
+	static int menuOpcionesFloristeria() {
+		Scanner entrada = new Scanner(System.in);
+		int opcion;
+		
+		System.out.println(" \n"
+				+ "OPCIONES DE FLORISTERIA \n"
+				+ "1.  Menú de productos. \n"
+				+ "2.  Mostrar stock. \n"
+				+ "3.  Mostrar cantidades en stock. \n"
+				+ "4.  Mostrar valor total de la floristería. \n"
+				+ "5.  Crear tickets. \n"
+				+ "6.  Mostrar ventas anteriores. \n"
+				+ "7.  Ganancias en ventas. \n"
+				+ "8.  Ver tickets anteriores. \n"
+				+ "0.  Volver al Menú. \n");
+		
+		opcion = Integer.parseInt(entrada.next());
+		
+		return opcion;
+	}
+	
+	static int menuProductos() {
+		Scanner entrada = new Scanner(System.in);
+		int opcion;
+		
+		System.out.println(" \n"
+				+ "OPCIONES DE PRODUCTO \n"
+				+ "1.  Añadir árbol. \n"
+				+ "2.  Retirar árbol.  \n"
+				+ "3.  Añadir decoración. \n"
+				+ "4.  Retirar decoración. \n"
+				+ "5.  Añadir Flor. \n"
+				+ "6.  Retirar flor. \n"
+				+ "7.  Ver ticket. \n"
+				+ "0.  Generar ticket y salir. \n");
+		
+		opcion = Integer.parseInt(entrada.next());
+		
+		return opcion;
+	}
+	
+	static int menuCantidadesStock() {
+		Scanner entrada = new Scanner(System.in);
+		
+		int opcion;
+		
+		System.out.println(" \n" 
+				+ "OPCIONES DE STOCK \n" 
+				+ "1. Mostrar stock de árboles. \n" 
+				+ "2. Mostrar stock de flores. \n"
+				+ "3. Mostrar stock de decoraciones. \n" 
+				+ "4. Mostrar todos los stocks. \n" 
+				+ "0. Volver al menú de floristería.");
+		
+		opcion = Integer.parseInt(entrada.next());
+		
+		return opcion;
+	}
+	
+	static int menuMostrarStock() {
+		Scanner entrada = new Scanner(System.in);
+		
+		int opcion;
+		
+		System.out.println(" \n" 
+				+ "OPCIONES DE MOSTRAR PRODUCTOS \n" 
+				+ "1. Mostrar árboles. \n" 
+				+ "2. Mostrar flores. \n"
+				+ "3. Mostrar decoraciones. \n" 
+				+ "4. Mostrar todos los productos. \n" 
+				+ "0. Volver al menú de floristería.");
+
+		opcion = Integer.parseInt(entrada.next());
+		
+		return opcion;
+	}
+}

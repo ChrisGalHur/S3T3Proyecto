c6a10ef91969441d65c45c0f5ce75f5d1d665b57
Ismael <Ismael.Tortosa@hotmail.com>
2022-12-15 11:04:55 +0100

    actu Main

--- src/Proyecto/Main.java
+++ src/Proyecto/Main.java
@@ -2,17 +2,13 @@ package Proyecto;
 
 import java.io.IOException;
 import java.util.List;
-import java.util.Scanner;
 
 public class Main {
 
 	
 	public static void main(String[] args) {
 
-		Scanner entrada = new Scanner(System.in);
-		
 		FloristeriaContenedor llamadaObjetosFloristeriaContenedor = new FloristeriaContenedor();
-
 
 		Floristeria floristeria;
 		int opcion = 0, opcionFloristeria = 0, opcionProductos = 0, opcionStocksCantidades = 0, opcionStocks = 0;
@@ -23,35 +19,30 @@ public class Main {
 				
 				switch (opcion) {
 				case 1:
-					
 					System.out.println("CREAR FLORISTERÍA");
 					llamadaObjetosFloristeriaContenedor.crearFloristeria();
 					break;
-					
 				case 2:
-					
 					System.out.println("MOSTRAR FLORISTERÍAS");
 					llamadaObjetosFloristeriaContenedor.mostrarFloristerias();
 					break;
-					
 				case 3:
-					
 					System.out.println("ELIMINAR FLORISTERÍA");
 					llamadaObjetosFloristeriaContenedor.eliminarFloristeria();
 					break;
-					
 				case 4:
-					
 					System.out.println("OPCIONES DE FLORISTERÍA");
 					floristeria = llamadaObjetosFloristeriaContenedor.devuelveFloristeria();
 					if(floristeria != null) {
 						do {
+							try {
 							opcionFloristeria = Menu.menuOpcionesFloristeria();
 							switch (opcionFloristeria) {
 								case 1:
 									Ticket ticket = new Ticket();
 									//Crear los tickets, compra
 									do {
+										try {
 										opcionProductos = Menu.menuProductos();
 										switch(opcionProductos) {
 											case 1:
@@ -93,35 +84,32 @@ public class Main {
 												break;
 											default:
 												System.out.println("Opción no válida");
+											}
+										}catch(Exception ex) {
+										System.out.println("ERROR de comando no válido.");
 										}
 									} while(opcionProductos != 0);
 									break;
 								case 2:
 									do {
+										try {
 										opcionStocks = Menu.menuMostrarStock();
 										
 										switch(opcionStocks) {
 										
 										case 1:
-											
 											System.out.println("INVENTARIO DE ÁRBOLES");
 											floristeria.mostrarArboles();
 											break;
-	
 										case 2:
-											
 											System.out.println("INVENTARIO DE FLORES");
 											floristeria.mostrarFlores();
 											break;
-	
 										case 3:
-											
 											System.out.println("INVENTARIO DE DECORACIONES");
 											floristeria.mostrarDecoraciones();
 											break;
-	
 										case 4:
-											
 											System.out.println("INVENTARIO DE ÁRBOLES");
 											floristeria.mostrarArboles();
 											System.out.println("INVENTARIO DE FLORES");
@@ -129,58 +117,50 @@ public class Main {
 											System.out.println("INVENTARIO DE DECORACIONES");
 											floristeria.mostrarDecoraciones();
 											break;
-	
 										case 0:
 											break;
-											
 										default:
 											System.out.println("Opción no válida");
 											break;
+											}
+										}catch(Exception ex) {
+											System.out.println("ERROR de comando no válido.");
 										}
-	
 									} while (opcionStocks != 0);
-								
-									break;
+								break;
 									
 								case 3:
-									
 									do {
-										
+										try {
 										opcionStocksCantidades = Menu.menuCantidadesStock();
 										
 										switch(opcionStocksCantidades) {
 										
 										case 1:
-											
 											System.out.println("STOCK DE ÁRBOLES");
 											floristeria.cantidadStockArboles();
 											break;
-	
 										case 2:
-											
 											System.out.println("STOCK DE FLORES");
 											floristeria.cantidadStockFlores();
 											break;
-	
 										case 3:
-											
 											System.out.println("STOCK DE DECORACIONES");
 											floristeria.cantidadStockDecoraciones();
 											break;
-	
 										case 4:
 											floristeria.cantidadStockArboles();
 											floristeria.cantidadStockFlores();
 											floristeria.cantidadStockDecoraciones();
 											break;
-	
 										case 0:
 											break;
-											
 										default:
 											System.out.println("Opción no válida");
 											break;
-											
+											}
+										}catch(Exception ex) {
+											System.out.println("ERROR de comando no válido.");
 										}
 	
 									} while (opcionStocksCantidades != 0);
@@ -201,6 +181,9 @@ public class Main {
 									break;
 								default:
 									System.out.println("Opción no válida");
+								}
+							}catch(Exception ex) {
+								System.out.println("ERROR de comando no válido.");
 							}
 						} while (opcionFloristeria != 0);
 					}
@@ -216,9 +199,4 @@ public class Main {
 			}
 		} while (opcion != 0);
 	}
-/*Corregir:
- * Limpiar el Buffer
- * enum decoracion
- * elimar ticket en ticket
- */
 }

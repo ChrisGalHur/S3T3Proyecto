ccdd2166925e9db292560d6ebb9784e6634ab54b
ChrisGalHur <108464294+ChrisGalHur@users.noreply.github.com>
2023-01-10 13:47:02 +0100

    Arreglos finales hechos

--- src/Proyecto/Flor.java
+++ src/Proyecto/Flor.java
@@ -1,11 +1,31 @@
 package Proyecto;
 
+/**
+ * Clase Flor. 
+ * 
+ * Contiene información del producto flor.
+ * 
+ * Es hija de la clase Producto.
+ * 
+ * @see Clase Producto.
+ * @author Ismael, Pilar y Christian.
+ *
+ */
 public class Flor extends Producto{
 	
 	//Atributos
+	/**
+	 * color del objeto flor.
+	 */
 	private String color;
 	
 	//Constructores
+	/**
+	 * Constructor con 3 parámetros
+	 * @param nombre tipo de flor.
+	 * @param precio precio de la flor.
+	 * @param color color de la flor.
+	 */
 	public Flor(String nombre, double precio, String color) {
 		super(nombre, precio);
 		this.color = color;
@@ -21,6 +41,9 @@ public class Flor extends Producto{
 		this.color = color;
 	}
 	
+	/**
+	 * información del objeto flor con sus atributos.
+	 */
 	@Override
 	public String toString() {
 		return "(id:" + super.getIdProducto() + ") Flor | Nombre: " + super.getNombre() + " | Precio: " + super.getPrecio() + "€ | Color: " + this.color;
--- src/Proyecto/Floristeria.java
+++ src/Proyecto/Floristeria.java
@@ -2,27 +2,57 @@ package Proyecto;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
 
+/**
+ * Clase Floristeria
+ * 
+ * Contiene métodos añadir, eliminar y mostrar los productos de la floristería (árboles, flores y decoraciones).
+ * También contiene métedos para mostrar las cantidades (stock) de los productos y calcular el valor total.
+ * Métodos guardar tickets y mostrarlos. 
+ * 
+ * @author Ismael, Pilar y Christian.
+ *
+ */
 public class Floristeria {
 
 	static Scanner entrada = new Scanner(System.in);
-
-	//Atributos
-	private String nombre;
+	
 	private List<Arbol> arboles = new ArrayList();
 	private List<Decoracion> decoraciones = new ArrayList();
 	private List<Flor> flores = new ArrayList();
+	
 	public Map<Integer, List<Producto>> tickets = new HashMap<>();
+	private List<String> nombreTickets = new ArrayList();
+
+	//Atributos
+	
+	/**
+	 * Nombre de la floristería.
+	 */
+	private String nombre;
+	/**
+	 * Número ID que se utilizará para el "autoincrement" para un ticket.
+	 */
 	private static int idStatic;
+	/**
+	 * Número de identificación ID de ticket. 
+	 */
 	private int idMap;
 	
 	//Constructores
+	/**
+	 * Constructor vacío.
+	 */
 	public Floristeria() {
 	}
-
+	/**
+	 * Constructor con 1 parámetro.
+	 * @param nombre nombre de la floristería.
+	 */
 	public Floristeria(String nombre) {
 		this.nombre = nombre;
 	}
@@ -58,16 +88,40 @@ public class Floristeria {
 		this.flores = flores;
 	}
 	
+	/**
+	 * Método para comprobar que la entrada del usuario sea un double.
+	 * @param missatge Pregunta al usuario.
+	 * @return retorna un double.
+	 */
+	public static double leerDouble(String missatge) {
+        double num = 0;
+        boolean seguir;
+            do {
+                try {
+                    seguir = false;
+                    System.out.println(missatge);
+                    num = entrada.nextDouble();
+                }catch(InputMismatchException e) {
+                    System.out.println("Introduzca un número.");
+                    entrada.next();
+                    seguir = true;
+                }
+                entrada.nextLine();
+            }while(seguir);
+        return num;
+    }
+	
 	//Método añadir arbol
+	/**
+	 * Crea un objeto árbol con sus atributos (nombre, precio y altura).
+	 * @return devuelve un objeto árbol.
+	 */
 	public Arbol anadirArbol() {
 		
 		System.out.println("Nombre del árbol: ");
 		String nombre = entrada.nextLine();
-		System.out.println("Precio del árbol: ");
-		double precio = entrada.nextDouble();
-		System.out.println("Altura del árbol: ");
-		double altura = entrada.nextDouble();
-		entrada.nextLine();//Limpiando Buffer
+		double precio = leerDouble("Precio del árbol: ");
+		double altura = leerDouble("altura del árbol: ");
 		
 		Arbol arbol = new Arbol(nombre,precio,altura);
 		arboles.add(arbol);
@@ -77,15 +131,17 @@ public class Floristeria {
 	}
 	
 	//Método añadir Flor
+	/**
+	 * Crea un objeto flor con sus atributos (nombre, precio y color).
+	 * @return Devuelve un objeto flor.
+	 */
 	public Flor anadirFlor() {
 		
 		System.out.println("Nombre de la flor: ");
 		String nombre = entrada.nextLine();
 		System.out.println("Color de la flor: ");
 		String color = entrada.nextLine();
-		System.out.println("Precio de la flor: ");
-		double precio = entrada.nextDouble();
-		entrada.nextLine();//Limpiando Buffer
+		double precio = leerDouble("Precio de añadir flor: ");
 		
 		Flor flor = new Flor(nombre,precio, color);
 		flores.add(flor);
@@ -94,19 +150,22 @@ public class Floristeria {
 	}
 	
 	//Método añadir Decoracion
+	/**
+	 * Crea un objecto decoración con sus atributos (nombre, precio y material).
+	 * Se da a elegir al usuario si desea la decoración en MADERA o PLÁSTICO mediante una clase ENUM.
+	 * @return Devuelve un objeto decoración.
+	 */
 	public Decoracion anadirDecoracion() {
 		
 		boolean elegido = false;
 		System.out.println("Nombre de la decoración: ");
 		String nombre = entrada.nextLine();
-		System.out.println("Precio de la decoración: ");
-		double precio = entrada.nextDouble();
-		entrada.nextLine();//Limpiando Buffer
+		
 		
 		String materiales ="";
 		do {
 			System.out.println("Elija material (Madera o Plástico) ");
-			materiales = entrada.next();
+			materiales = entrada.next().toUpperCase();
 			if(materiales.equalsIgnoreCase("madera") || materiales.equalsIgnoreCase("plástico")) {
 				elegido = true;
 			}
@@ -114,6 +173,8 @@ public class Floristeria {
 				System.out.println("Opcion no correcta.");
 			}
 		}while(!elegido);
+		
+		double precio = leerDouble("Precio de la decoración: ");
 		
 		Decoracion decoracion = new Decoracion(nombre, precio, materiales);
 		decoraciones.add(decoracion);
@@ -123,6 +184,10 @@ public class Floristeria {
 	
 
 	// método ver árboles
+	/**
+	 * Muestra todos los objetos árboles.
+	 * Si no hay árboles creados mostrará un mensaje de tipo error.
+	 */
 	public void mostrarArboles() {
 		System.out.println("STOCK DE ÁRBOLES");
 		
@@ -136,6 +201,10 @@ public class Floristeria {
 	}
 
 	// método ver flores
+	/**
+	 * Muestra todos los objetos flores.
+	 * Si no hay flores creadas mostrará un mensaje de tipo error.
+	 */
 	public void mostrarFlores() {
 		System.out.println("STOCK DE FLORES");
 		
@@ -149,6 +218,10 @@ public class Floristeria {
 	}
 
 	// método ver decoraciones
+	/**
+	 * Muestra todos los objetos decoración.
+	 * Si no hay decoraciones creadas mostrará un mensaje de tipo error.
+	 */
 	public void mostrarDecoraciones() {
 		System.out.println("STOCK DE DECORACIONES");
 		
@@ -163,14 +236,20 @@ public class Floristeria {
 	}
 
 	//método cantidades árboles
+	/**
+	 * Muestra la cantidad existente de objetos árbol.
+	 */
 	public void cantidadStockArboles() {
 		
 		int cantidadArboles = arboles.size();
 		
-		System.out.println("Hay:" + cantidadArboles + " árboles.");
+		System.out.println("Hay: " + cantidadArboles + " árboles.");
 	}
 	
 	//método cantidades flores
+	/**
+	 * Muestra la cantidad existente de objetos flor.
+	 */
 	public void cantidadStockFlores() {
 		
 		int cantidadFlores = flores.size();
@@ -179,6 +258,9 @@ public class Floristeria {
 	}
 	
 	//método cantidades decoraciones
+	/**
+	 * Muestra la cantidad existente de objetos decoración.
+	 */
 	public void cantidadStockDecoraciones() {
 		
 		int cantidadDecoraciones = decoraciones.size();
@@ -187,10 +269,18 @@ public class Floristeria {
 	}
 
 	//Método eliminar Arbol
-	public void eliminarArbol() {
+	/**
+	 * Elimina un objeto árbol mediante una identificación númerica tipo ID.
+	 * <ul>
+	 * <li> true: se elimina el objeto </li>
+	 * <li> false: no se ha eliminado el objeto </li>
+	 * </ul>
+	 * @return retorna el objeto eliminado.
+	 */
+	public Arbol eliminarArbol() {
 		int idEliminar;
 		boolean eliminarHecho = false;
-		
+		Arbol arbol = null;
 		mostrarArboles();
 		if(arboles.size() > 0) {
 			System.out.println("Qué ID de Arbol quieres que eliminemos?");
@@ -198,6 +288,7 @@ public class Floristeria {
 			//Buscamos la coincidencia de id y si la hay la marcamos en eliminarHecho
 			for(int i = 0; i < arboles.size(); i++) {
 				if (arboles.get(i).getIdProducto() == idEliminar){
+					arbol = arboles.get(i);
 					arboles.remove(i);
 					eliminarHecho = true;
 				}
@@ -209,56 +300,82 @@ public class Floristeria {
 				System.out.println("No se ha podido eliminar el Arbol.");
 			}
 		}
+		return arbol;
 	}
 	
 	//Método eliminar Flor
-	public void eliminarFlor() {
-		int idEliminar;
-		boolean eliminarHecho = false;
-			
-		mostrarArboles();
-		System.out.println("Qué ID de Flor quieres que eliminemos?");
-		idEliminar = entrada.nextInt();
-				
-		for(int i = 0; i < flores.size(); i++) {
-			if (flores.get(i).getIdProducto() == idEliminar){
-				flores.remove(i);
-				eliminarHecho = true;
+	/**
+	 * Elimina un objeto flor mediante una identificación númerica tipo ID.
+	 * <ul>
+	 * <li> true: se elimina el objeto </li>
+	 * <li> false: no se ha eliminado el objeto </li>
+	 * </ul>
+	 * @return retorna el objeto eliminado.
+	 */
+		public Flor eliminarFlor() {
+			int idEliminar;
+			boolean eliminarHecho = false;
+			Flor flor = null;
+			mostrarArboles();
+			System.out.println("Qué ID de Flor quieres que eliminemos?");
+			idEliminar = entrada.nextInt();
+					
+			for(int i = 0; i < flores.size(); i++) {
+				if (flores.get(i).getIdProducto() == idEliminar){
+					flores.get(i);
+					flores.remove(i);
+					eliminarHecho = true;
+				}
 			}
+			
+			if(eliminarHecho == true){
+				System.out.println("La Flor se ha eliminado correctamente.");
+			}else{
+				System.out.println("No se ha podido eliminar la Flor.");
+			}
+			return flor;
 		}
 		
-		if(eliminarHecho == true){
-			System.out.println("La Flor se ha eliminado correctamente.");
-		}else{
-			System.out.println("No se ha podido eliminar la Flor.");
-		}
-	}
-	
-	//Método eliminar Decoración
-	public void eliminarDecoracion() {
-		int idEliminar;
-		boolean eliminarHecho = false;
-			
-		mostrarArboles();
-		System.out.println("Qué ID de Arbol quieres que eliminemos?");
-		idEliminar = entrada.nextInt();
-				
-		for(int i = 0; i < decoraciones.size(); i++) {
-			if (decoraciones.get(i).getIdProducto() == idEliminar){
-				decoraciones.remove(i);
-				eliminarHecho = true;
+		//Método eliminar Decoración
+		/**
+		 * Elimina un objeto decoración mediante una identificación númerica tipo ID.
+		 * <ul>
+		 * <li> true: se elimina el objeto </li>
+		 * <li> false: no se ha eliminado el objeto </li>
+		 * </ul>
+		 * @return retorna el objeto eliminado.
+		 */
+		public Decoracion eliminarDecoracion() {
+			int idEliminar;
+			boolean eliminarHecho = false;
+			Decoracion decoracion = null;	
+			mostrarArboles();
+			System.out.println("Qué ID de Arbol quieres que eliminemos?");
+			idEliminar = entrada.nextInt();
+					
+			for(int i = 0; i < decoraciones.size(); i++) {
+				if (decoraciones.get(i).getIdProducto() == idEliminar){
+					decoraciones.get(i);
+					decoraciones.remove(i);
+					eliminarHecho = true;
+				}
 			}
+			
+			if(eliminarHecho == true){
+				System.out.println("La Decoración se ha eliminado correctamente.");
+			}else{
+				System.out.println("No se ha podido eliminar la Decoración.");
+			}
+			return decoracion;
 		}
-		
-		if(eliminarHecho == true){
-			System.out.println("La Decoración se ha eliminado correctamente.");
-		}else{
-			System.out.println("No se ha podido eliminar la Decoración.");
-		}
-	}
+
 
 	//Metodos de valores
-	public void calcularValorTotal() {
+	/**
+	 * Calcula el valor total de los productos de la floristería.
+	 * @return Valor total
+	 */
+	public double calcularValorTotal() {
 		double valorTotal = 0,valorTotalArboles = 0, valorTotalFlores = 0, valorTotalDecoraciones = 0;
 		
 		//Valor en arboles 
@@ -266,7 +383,7 @@ public class Floristeria {
 			valorTotalArboles = valorTotalArboles + arboles.get(i).getPrecio();
 		}
 		//Valor en flores
-		for(int i = 0; i < arboles.size(); i++) {
+		for(int i = 0; i < flores.size(); i++) {
 			valorTotalFlores = valorTotalFlores + flores.get(i).getPrecio();
 		}
 		//Valor en decoraciones
@@ -277,32 +394,83 @@ public class Floristeria {
 		valorTotal = valorTotalArboles + valorTotalFlores + valorTotalDecoraciones;
 		//Hago los calculos por separado por si modificamos tenerlos ya
 		
-		System.out.println("El valor actual de " + nombre + " es de " + valorTotal + "€.");
+		System.out.println(nombre + " es de " + valorTotal + "€.");
+		
+		return valorTotal;
+		
+		
 	}
 
-	//Método guardar tiquet
-	public void almacenarTicket(List <Producto> producto) {
+	/**
+	 * Se almacena en un map el arrayList de productos
+	 * 
+	 * @param producto
+	 */
+	public void almacenarTicket(List <Producto> productos) {
         idStatic++;
         idMap = idStatic;
-        tickets.put(idMap, producto);
+        tickets.put(idMap, productos);
     }
 
-	//Método ver tiquets
+	/**
+	 * Se recorre el map tickets
+	 */
     public void verTickets() {
         if(tickets.size()== 0) {
             System.out.println("No existen tickets guardados");
         }else {
             for(Map.Entry entry: tickets.entrySet()) {
-                System.out.println(entry.getKey() +" "+ entry.getValue());
+                System.out.println("Ticket " + entry.getKey() +" "+ entry.getValue());
             }
         }
     }
 	
+    /**
+     * Se almacena en un arrayList el nombre de los
+     * tickets impresos
+     * 
+     * @param nombreTicket
+     */
+    public void almacenarNombreTicketsImpresos(String nombreTicket) {
+    	nombreTickets.add(nombreTicket);
+    }
+    
+    /**
+     * Lee el archivo .txt que desea el usuario.
+     */
+    public void verNombreTicketsImpresos() {
+		Scanner entrada = new Scanner(System.in);
+		
+		boolean existe = false;
+		
+		System.out.println("Qué ticket desea comprobar?");
+		
+    	nombreTickets.forEach(e-> System.out.println(e));
+    	
+    	String recogerNombreArchivo = entrada.nextLine();
+    	
+    	for (int i=0; i < nombreTickets.size();i++) {
+    		if(nombreTickets.get(i).equals(recogerNombreArchivo)) {
+    			existe = true;
+    		}
+    	}
+    	
+    	if(existe) {
+    		Archivo.leerTicket(recogerNombreArchivo);
+    	}else {
+    		System.out.println("El archivo no existe");
+    	}
+    	
+    }
+    
+    /**
+     * Devuelve una cadena que representa al objeto floristería.
+     * @return información de la floristería con su único atributo nombre.
+     */
 	@Override
 	public String toString() {
 		return "Floristeria: " + this.nombre ;
 
 	}
-	
 	
 }
